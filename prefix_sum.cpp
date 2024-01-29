ffbdc8cfaf42a9e1ae7aaa8cedd2998b0e1906f8
Michael Kosiorek <97412062+michaelkosiorek@users.noreply.github.com>
2024-01-28 21:55:19 -0500

    Update prefix_sum.cpp

--- prefix_sum.cpp
+++ prefix_sum.cpp
@@ -10,19 +10,34 @@ int prefix_sum(int ary[], int length) {
         sum += ary[i];
     }
 
-
     return sum;
 }
 
 
+// returns true if prefix sum of ary is never negative
 bool non_neg_prefix_sum(int ary[], int length) {
 
     int sum=0;
 
     for(int i=0; i<length; i++) {
+        // std::cout << "Entered loop with ary len: " << length << std::endl;
         sum += ary[i];
+        if (sum < 0) return false;
     }
 
+    return true;
+}
 
-    return sum;
+// returns true if prefix sum of ary is never positive
+bool non_pos_prefix_sum(int ary[], int length) {
+
+    int sum=0;
+
+    for (int i=0; i < length; i++) {
+        // std::cout << "Entered loop with ary len: " << length << std::endl;
+        sum += ary[i];
+        if (sum > 0) return false;
+    }
+
+    return true;
 }
